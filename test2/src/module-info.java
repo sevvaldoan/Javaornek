308009946266ef68c8183cfb0b82efd50480d50d
sewal <sewal@SevvalDogan>
2022-10-03 18:51:14 +0300

    İlk commit

---
+++ test2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sewal
+ *
+ */
+module test2 {
+}
---
+++ test2/src/test2/Test.java
@@ -0,0 +1,9 @@
+package test2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.print("Sevval");
+	}
+
+}
